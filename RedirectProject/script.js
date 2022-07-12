fb9c6650f20458b95b350f3998f7586c15bfa56f
André Godeiro <andrevictor.godeiro@gmail.com>
2022-07-12 18:08:00 -0300

    first commit

---
+++ RedirectProject/script.js
@@ -0,0 +1,6 @@
+var numero = document.getElementById("inputNumero");
+
+document.getElementById("botao").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = `https://api.whatsapp.com/send?phone=55${numero}&text=232323232323`;
+});

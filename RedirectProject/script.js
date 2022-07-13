31953cc7bd41d237709134ce947376041da71073
André Godeiro <andrevictor.godeiro@gmail.com>
2022-07-12 23:39:03 -0300

    script update - open in a new tab

--- RedirectProject/script.js
+++ RedirectProject/script.js
@@ -4,6 +4,9 @@ var especialidade = document.getElementById("inputEspecialidade");
 
 document.getElementById("botao").addEventListener("click", function (event) {
   event.preventDefault();
-  window.location.href = `https://api.whatsapp.com/send?phone=55${numero.value}&text=Olá, ${cliente.value}, tudo bem? Me chamo André Godeiro, sou Advogado. Estou entrando em contato pois o JusBrasil me repassou o seu caso.
-  Trata-se de ${especialidade.value}, correto?`;
+  window.open(
+    `https://api.whatsapp.com/send?phone=55${numero.value}&text=Olá, ${cliente.value}, tudo bem? Me chamo André Godeiro, sou Advogado. Estou entrando em contato pois o JusBrasil me repassou o seu caso.
+  Trata-se de ${especialidade.value}, correto?`,
+    "_blank"
+  );
 });

43c0435461f8616a2eececadcee7def1592ecac8
André Godeiro <andrevictor.godeiro@gmail.com>
2022-07-12 23:30:36 -0300

    html and javascript improvements

--- RedirectProject/script.js
+++ RedirectProject/script.js
@@ -1,6 +1,9 @@
 var numero = document.getElementById("inputNumero");
+var cliente = document.getElementById("inputCliente");
+var especialidade = document.getElementById("inputEspecialidade");
 
 document.getElementById("botao").addEventListener("click", function (event) {
   event.preventDefault();
-  window.location.href = `https://api.whatsapp.com/send?phone=55${numero.value}&text=232323232323`;
+  window.location.href = `https://api.whatsapp.com/send?phone=55${numero.value}&text=Olá, ${cliente.value}, tudo bem? Me chamo André Godeiro, sou Advogado. Estou entrando em contato pois o JusBrasil me repassou o seu caso.
+  Trata-se de ${especialidade.value}, correto?`;
 });

564f11839edad9d593e31cc4f799c48011b4dbe7
André Godeiro <andrevictor.godeiro@gmail.com>
2022-07-22 23:41:24 -0300

    input number update

--- RedirectProject/script.js
+++ RedirectProject/script.js
@@ -2,10 +2,23 @@ var numero = document.getElementById("inputNumero");
 var cliente = document.getElementById("inputCliente");
 var especialidade = document.getElementById("inputEspecialidade");
 
+const removeMask = (value) => {
+  if (value) {
+    const stringValue = value.toString();
+    const regex = /[R$()-.,/% ]/gi;
+    return stringValue.replace(regex, "");
+  }
+};
+
 document.getElementById("botao").addEventListener("click", function (event) {
   event.preventDefault();
+  console.log(numero.value);
   window.open(
-    `https://api.whatsapp.com/send?phone=55${numero.value}&text=Olá, ${cliente.value}, tudo bem? Me chamo André Godeiro, sou Advogado. Estou entrando em contato pois o JusBrasil me repassou o seu caso.
+    `https://api.whatsapp.com/send?phone=55${removeMask(
+      numero.value
+    )}&text=Olá, ${
+      cliente.value
+    }, tudo bem? Me chamo André Godeiro, sou Advogado. Estou entrando em contato pois o JusBrasil me repassou o seu caso.
   Trata-se de ${especialidade.value}, correto?`,
     "_blank"
   );
